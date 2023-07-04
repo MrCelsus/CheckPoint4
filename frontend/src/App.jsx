c9fa2d15f7786ac9a0d14ac808a31cff67fa540e
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-04 21:47:37 +0200

    Nettoyage du template et installation de react router + sass

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import Home from "./pages/Home";
-
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <p>coucou</p>
     </div>
   );
