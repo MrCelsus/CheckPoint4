afe44ed1e7abbed7b748c4f616215727d0c50b57
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 18:27:39 +0200

    Création de la fonction read

--- backend/src/controllers/faqsControllers.js
+++ backend/src/controllers/faqsControllers.js
@@ -12,6 +12,23 @@ const browse = (req, res) => {
     });
 };
 
+const read = (req, res) => {
+  models.faqs
+    .find(req.params.id)
+    .then(([faqs]) => {
+      if (faqs[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(faqs[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   browse,
+  read,
 };
