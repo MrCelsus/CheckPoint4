0f2589009d4bca205d7c96f19ff3f890ade536e7
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 21:33:20 +0200

    Regroupement et récupération de mes images

--- backend/src/models/CarsManager.js
+++ backend/src/models/CarsManager.js
@@ -7,13 +7,13 @@ class CarsManager extends AbstractManager {
 
   findAll() {
     return this.database.query(
-      `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id`
+      `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label, JSON_ARRAYAGG(JSON_OBJECT('image_id', ci.id, 'src', ci.src)) as images from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id inner join car_images ci on c.id=ci.car_id group by c.id`
     );
   }
 
   find(id) {
     return this.database.query(
-      `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id where c.id = ?`,
+      `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label, JSON_ARRAYAGG(JSON_OBJECT('image_id', ci.id, 'src', ci.src)) as images from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id inner join car_images ci on c.id=ci.car_id where c.id = ?`,
       [id]
     );
   }
