0b587b5cddd33cfaa3d9dcba065fbad2df158f93
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 18:21:56 +0200

    Création de la fonction find

--- backend/src/models/CarsManager.js
+++ backend/src/models/CarsManager.js
@@ -10,6 +10,13 @@ class CarsManager extends AbstractManager {
       `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id`
     );
   }
+
+  find(id) {
+    return this.database.query(
+      `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id where c.id = ?`,
+      [id]
+    );
+  }
 }
 
 module.exports = CarsManager;
