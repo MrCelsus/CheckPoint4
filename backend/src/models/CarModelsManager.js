f0696a42b374690728e31540c720e6ee570e08a7
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 18:41:43 +0200

    Modification du find pour récupérer la data nécessaire

--- backend/src/models/CarModelsManager.js
+++ backend/src/models/CarModelsManager.js
@@ -12,6 +12,13 @@ class CarModelsManager extends AbstractManager {
     }
     return this.database.query(url, [brand]);
   }
+
+  find(id) {
+    return this.database.query(
+      `select m.id, m.model, b.brand from  ${this.table} m inner join brands b on m.brand_id=b.id where m.id = ?`,
+      [id]
+    );
+  }
 }
 
 module.exports = CarModelsManager;
