2f45cd870e4880eda105db89349ff2509bdd5c33
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 17:56:50 +0200

    Modification de la requete pour récupérer tout les datas nécessaires en fonction de l'id des tables qui y sont rattachées

--- backend/src/models/CarsManager.js
+++ backend/src/models/CarsManager.js
@@ -4,6 +4,12 @@ class CarsManager extends AbstractManager {
   constructor() {
     super({ table: "cars" });
   }
+
+  findAll() {
+    return this.database.query(
+      `select c.id, c.fiscal_power, c.motor_power, c.kilometers, c.description, c.price, m.model, b.brand, ext.label ext_label, interiors.label int_label, f.label fuel_label from ${this.table} c inner join car_models m on c.model_id=m.id inner join brands b on m.brand_id=b.id inner join externals ext on c.external_id=ext.id inner join interiors on c.interior_id=interiors.id inner join fuels f on c.fuel_id=f.id`
+    );
+  }
 }
 
 module.exports = CarsManager;
