8bf13abd8e7b21258dc049de9d3330a3e1c87190
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 22:08:49 +0200

    Création de la fonction insert pour la FAQ

--- backend/src/models/FaqsManager.js
+++ backend/src/models/FaqsManager.js
@@ -4,6 +4,13 @@ class FaqsManager extends AbstractManager {
   constructor() {
     super({ table: "faqs" });
   }
+
+  insert(question, answer) {
+    return this.database.query(
+      `insert into ${this.table} (question, answer) values (?, ?)`,
+      [question, answer]
+    );
+  }
 }
 
 module.exports = FaqsManager;
