5bc6ad4da8b1009dd9ed4d2ed5bda2ad7478d97e
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 21:48:36 +0200

    Création de la fonction insert de la marque + logo

--- backend/src/models/BrandsManager.js
+++ backend/src/models/BrandsManager.js
@@ -4,6 +4,13 @@ class BrandsManager extends AbstractManager {
   constructor() {
     super({ table: "brands" });
   }
+
+  insert(brand, logo) {
+    return this.database.query(
+      `insert into ${this.table} (brand, logo) values (?,?)`,
+      [brand, logo]
+    );
+  }
 }
 
 module.exports = BrandsManager;
