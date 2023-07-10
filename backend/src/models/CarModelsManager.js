082dede6d6acc8370b66fcad83b4ecf0c315c15c
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 17:32:08 +0200

    Récupérartion de certains modèles de voiture en fonction de la marque ou de tous mes modèles

--- backend/src/models/CarModelsManager.js
+++ backend/src/models/CarModelsManager.js
@@ -4,6 +4,14 @@ class CarModelsManager extends AbstractManager {
   constructor() {
     super({ table: "car_models" });
   }
+
+  findAllByBrand(brand) {
+    let url = `select m.id, m.model, b.brand from  ${this.table} m inner join brands b on m.brand_id=b.id `;
+    if (brand) {
+      url += ` where b.brand = ? `;
+    }
+    return this.database.query(url, [brand]);
+  }
 }
 
 module.exports = CarModelsManager;
