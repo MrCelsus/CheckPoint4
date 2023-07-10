61e299b08f7f1cde6d721f833b508b6f4ca32894
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 16:42:44 +0200

    Importation de CarModels dans index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -39,6 +39,11 @@ const BrandsManager = require("./BrandsManager");
 models.brands = new BrandsManager();
 models.brands.setDatabase(pool);
 
+const CarModelsManager = require("./CarModelsManager");
+
+models.carModels = new CarModelsManager();
+models.carModels.setDatabase(pool);
+
 // bonus: use a proxy to personalize error message,
 // when asking for a non existing model
 
