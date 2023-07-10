35c74568f691351990328b607431fc776892e2a5
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-10 16:28:33 +0200

    Importation de BrandsManager dans index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -34,6 +34,11 @@ const ItemManager = require("./ItemManager");
 models.item = new ItemManager();
 models.item.setDatabase(pool);
 
+const ItemManager = require("./ItemManager");
+
+models.brands = new BrandsManager();
+models.brands.setDatabase(pool);
+
 // bonus: use a proxy to personalize error message,
 // when asking for a non existing model
 
