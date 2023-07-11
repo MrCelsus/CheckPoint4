a6c611ac6cc6e24d31168d3b5db1b980a05b2893
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-11 16:07:37 +0200

    Création de la fonction destroy

--- backend/src/controllers/carsControllers.js
+++ backend/src/controllers/carsControllers.js
@@ -89,9 +89,26 @@ const edit = (req, res) => {
     });
 };
 
+const destroy = (req, res) => {
+  models.cars
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   browse,
   read,
   add,
   edit,
+  destroy,
 };
