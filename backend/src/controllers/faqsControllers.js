22ba78f6bb01ec95ace8c837721b4c8f04353cf4
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-11 12:30:38 +0200

    Création de la fonction edit pour la FAQ

--- backend/src/controllers/faqsControllers.js
+++ backend/src/controllers/faqsControllers.js
@@ -31,8 +31,6 @@ const read = (req, res) => {
 const add = (req, res) => {
   const { question, answer } = req.body;
 
-  // TODO validations (length, format...)
-
   models.faqs
     .insert(question, answer)
     .then(([result]) => {
@@ -44,8 +42,28 @@ const add = (req, res) => {
     });
 };
 
+const edit = (req, res) => {
+  const faq = req.body;
+  const faqId = parseInt(req.params.id, 10);
+
+  models.faqs
+    .update(faq, faqId)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   browse,
   read,
   add,
+  edit,
 };
