f04c6b2acafd2f279d0cc0788b259309fe5cc3c2
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-12 08:00:20 +0200

    Importation de mon Layout User

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import AdminLayout from "./Admin/AdminLayout";
+import UserLayout from "./User/UserLayout";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          {/* Route User */}
+          <Route path="/" element={<UserLayout />} />
           {/* Route Admin */}
-          <Route path="/admin/" element={<AdminLayout />}>
-            <Route />
-          </Route>
+          <Route path="/admin/" element={<AdminLayout />} />
         </Routes>
       </Router>
     </div>
