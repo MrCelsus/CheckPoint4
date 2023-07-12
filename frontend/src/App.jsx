d25db0458b49e5583394095fdd9c46e0673e549d
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-12 07:46:00 +0200

    Importation de mon Layout Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
+import AdminLayout from "./Admin/AdminLayout";
 
 function App() {
   return (
     <div className="App">
-      <p>coucou</p>
+      <Router>
+        <Routes>
+          {/* Route Admin */}
+          <Route path="/admin/" element={<AdminLayout />}>
+            <Route />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
