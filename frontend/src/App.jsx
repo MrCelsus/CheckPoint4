42493464b560841f89b8a07a3dcb3cb279d6d1e2
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-14 10:33:21 +0200

    Import de la Siebar dans App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./Sass/App.scss";
 import AdminLayout from "./pages/Admin/AdminLayout";
 import UserLayout from "./pages/User/UserLayout";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <div className="App">
+    <header className="App">
       <Router>
         <Routes>
           {/* Route User */}
           <Route path="/" element={<UserLayout />} />
           {/* Route Admin */}
-          <Route path="/admin/" element={<AdminLayout />} />
+          <Route path="/admin/" element={<AdminLayout />}>
+            <Route path="" element={<SideBar />} />
+          </Route>
         </Routes>
       </Router>
-    </div>
+    </header>
   );
 }
 
