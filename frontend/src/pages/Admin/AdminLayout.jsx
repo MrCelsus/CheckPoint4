a1c2db531d520586ae9735fa86845912df9f2769
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-14 10:32:54 +0200

    Ajout de Outlet dans Admin Layout

--- frontend/src/pages/Admin/AdminLayout.jsx
+++ frontend/src/pages/Admin/AdminLayout.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function AdminLayout() {
-  return <div>AdminLayout</div>;
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
 }
 
 export default AdminLayout;
