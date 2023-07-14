ccd99fd453e5d589fd795033fc60b2a6533a64a2
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-14 10:43:52 +0200

    Link des mes pages admin

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <aside>
+      <Link to="/admin/"> Accueil</Link>
+      <Link to="/admin/cars"> Liste des voitures</Link>
+      <Link to="/admin/brands"> Liste des marques</Link>
+      <Link to="/admin/profils">Liste des utilisateurs</Link>
+    </aside>
+  );
 }
 
 export default SideBar;
