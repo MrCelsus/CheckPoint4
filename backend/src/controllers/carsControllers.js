dc9ff833ad56014943e852254026499e96e1614e
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-19 16:06:05 +0200

    Réorganisation de la fonction add + update de ma voiture et/ou de mes images

--- backend/src/controllers/carsControllers.js
+++ backend/src/controllers/carsControllers.js
@@ -30,38 +30,20 @@ const read = (req, res) => {
 
 const add = async (req, res) => {
   const cars = req.body;
-  const {
-    modelId,
-    fiscalPower,
-    motorPower,
-    kilometers,
-    description,
-    price,
-    externalId,
-    interiorId,
-    fuelId,
-    src1,
-    src2,
-    src3,
-  } = cars;
   try {
-    const car = await models.cars.insert(
-      modelId,
-      fiscalPower,
-      motorPower,
-      kilometers,
-      description,
-      price,
-      externalId,
-      interiorId,
-      fuelId,
-      src1,
-      src2,
-      src3
+    const car = await models.cars.insert(cars);
+    const image1 = await models.carImages.insert(
+      cars.images[0].src,
+      car[0].insertId
     );
-    const image1 = await models.carImages.insert(src1, car[0].insertId);
-    const image2 = await models.carImages.insert(src2, car[0].insertId);
-    const image3 = await models.carImages.insert(src3, car[0].insertId);
+    const image2 = await models.carImages.insert(
+      cars.images[1].src,
+      car[0].insertId
+    );
+    const image3 = await models.carImages.insert(
+      cars.images[2].src,
+      car[0].insertId
+    );
     res.location(`/cars/${car[0].insertId}`);
     res.location(`/images/${image1[0].insertId}`);
     res.location(`/images/${image2[0].insertId}`);
@@ -73,24 +55,20 @@ const add = async (req, res) => {
   }
 };
 
-const edit = (req, res) => {
-  const car = req.body;
-
+const edit = async (req, res) => {
+  const cars = req.body;
+  const { images } = cars;
   const carId = parseInt(req.params.id, 10);
-
-  models.cars
-    .update(car, carId)
-    .then(([cars]) => {
-      if (cars.affectedRows === 0) {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(204);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
+  try {
+    await models.cars.update(cars, carId);
+    await models.carImages.update(images[0]);
+    await models.carImages.update(images[1]);
+    await models.carImages.update(images[2]);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 };
 
 const destroy = (req, res) => {
