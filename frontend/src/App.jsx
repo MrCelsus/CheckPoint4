1d26674f4223125c22c7ebc5eb3c6c2d1bff43c9
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-19 09:40:22 +0200

    Imporation de AdminCars

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import AdminLayout from "./Admin/AdminLayout";
 import UserLayout from "./User/UserLayout";
+import CarsAdmin from "./Admin/pages/CarsAdmin";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
           {/* Route User */}
           <Route path="/" element={<UserLayout />} />
           {/* Route Admin */}
-          <Route path="/admin/" element={<AdminLayout />} />
+          <Route path="/admin/" element={<AdminLayout />}>
+            <Route path="cars" element={<CarsAdmin />} />
+          </Route>
         </Routes>
       </Router>
     </div>
