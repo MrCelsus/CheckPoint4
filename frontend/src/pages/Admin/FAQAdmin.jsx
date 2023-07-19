f14ab90afe4c34a84eaca8e2549beb1766b4a53e
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2023-07-19 16:47:01 +0200

    Récupération de ma FAQ + map des question dans un select

--- frontend/src/pages/Admin/FAQAdmin.jsx
+++ frontend/src/pages/Admin/FAQAdmin.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import connexion from "../../services/connexion";
 
 function FAQAdmin() {
-  return <div>FAQAdmin</div>;
+  const [faqs, setFaqs] = useState([]);
+  const getFaqs = async () => {
+    try {
+      const faqsData = await connexion.get("/faqs");
+      if (faqsData) {
+        setFaqs(faqsData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getFaqs();
+  }, []);
+  return (
+    <main>
+      <h1>Liste des Questions/Réponses</h1>
+      <select name="faqs" id="">
+        <option value=""> choisir une question</option>
+        {faqs.map((faq) => (
+          <option value={faq.id} key={faq.id}>
+            {faq.question}
+          </option>
+        ))}
+      </select>
+    </main>
+  );
 }
 
 export default FAQAdmin;
