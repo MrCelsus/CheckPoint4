3f322ae7541ddc61aef9ddbdaa69b5e476f6d0df
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2023-07-20 21:51:49 +0200

    Importation d'icone + Suppression des pages non traités

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { FaCar } from "react-icons/fa";
+import {
+  BsQuestionCircle,
+  BsReverseLayoutTextSidebarReverse,
+} from "react-icons/bs";
 
 function SideBar() {
   return (
-    <aside>
-      <Link to="/admin/"> Accueil</Link>
-      <Link to="/admin/cars"> Liste des voitures</Link>
-      <Link to="/admin/brands"> Liste des marques</Link>
-      <Link to="/admin/profils">Liste des utilisateurs</Link>
-      <Link to="/admin/faq">Liste des questions</Link>
+    <aside className="sidebar">
+      <h1>Welcome Back Admin</h1>
+      <Link to="/admin/" className="home-cms">
+        <BsReverseLayoutTextSidebarReverse size={20} /> Accueil CMS
+      </Link>
+      <Link to="/admin/cars">
+        <FaCar size={20} /> Liste des voitures
+      </Link>
+      <Link to="/admin/faq">
+        <BsQuestionCircle size={20} /> Liste des questions
+      </Link>
+      <Link to="/" className="home-cms">
+        <BiHomeAlt2 size={20} /> Retour sur le site
+      </Link>
     </aside>
   );
 }
