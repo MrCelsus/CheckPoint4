db68186641cd9e060b9cada87aac4d6d6d2ff076
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2023-07-20 14:51:48 +0200

    Création du Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h4>Mentions Légales</h4>
+      <h4>Politique de confidentialités</h4>
+    </footer>
+  );
+}
+
+export default Footer;
