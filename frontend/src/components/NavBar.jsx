583f6abd8c1bdc607abae626409f91f032aabe84
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2023-07-20 12:31:41 +0200

    Création de la navBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { BiHome } from "react-icons/bi";
+import { RxHamburgerMenu } from "react-icons/rx";
+import {
+  BsQuestionCircle,
+  BsReverseLayoutTextSidebarReverse,
+  BsDownload,
+} from "react-icons/bs";
+
+import Logo from "../assets/Logo_Blanc_Bleu.jpg";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const whiteLogo = {
+    color: "white",
+  };
+  return (
+    <nav className="navbar">
+      <img src={Logo} alt="Jeun'Automobile" className="logo-container" />
+      <button type="button" className="burger-menu" onClick={toggleMenu}>
+        <RxHamburgerMenu size={42} style={whiteLogo} />
+      </button>
+      <div className={`menu ${showMenu ? "show" : ""}`}>
+        <Link to="/">
+          <h4>Home</h4>
+          <span>
+            <BiHome size={25} />
+          </span>
+        </Link>
+        <Link to="/faq">
+          <h4>F.A.Q</h4>
+          <span>
+            <BsQuestionCircle size={25} />
+          </span>
+        </Link>
+        <Link to="/signin">
+          <h4>Log Out</h4>
+          <span>
+            <BsDownload size={25} style={{ rotate: "-90deg" }} />
+          </span>
+        </Link>
+        <Link to="/admin">
+          <h4>CMS</h4>
+          <span>
+            <BsReverseLayoutTextSidebarReverse size={25} />
+          </span>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
