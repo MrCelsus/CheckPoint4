11ca2d169114912924297a0e6fa735acc4975a9c
Lucas DEGRET <126179037+MrCelsus@users.noreply.github.com>
2023-07-20 23:16:31 +0200

    Suppression des imports  des pages non traités

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./Sass/App.scss";
 // Admin
 import AdminLayout from "./pages/Admin/AdminLayout";
-import HomeAdmin from "./pages/Admin/HomeAdmin";
 import CarsAdmin from "./pages/Admin/CarsAdmin";
-import BrandsAdmin from "./pages/Admin/BrandsAdmin";
-import ProfilsAdmin from "./pages/Admin/ProfilsAdmin";
 import FAQAdmin from "./pages/Admin/FAQAdmin";
 // User
 import UserLayout from "./pages/User/UserLayout";
@@ -26,10 +23,7 @@ function App() {
           </Route>
           {/* Routes Admin */}
           <Route path="/admin/" element={<AdminLayout />}>
-            <Route path="" element={<HomeAdmin />} />
-            <Route path="cars" element={<CarsAdmin />} />
-            <Route path="brands" element={<BrandsAdmin />} />
-            <Route path="profils" element={<ProfilsAdmin />} />
+            <Route path="" element={<CarsAdmin />} />
             <Route path="faq" element={<FAQAdmin />} />
           </Route>
         </Routes>
